11055c3096a6afc495c4ef3881c09d53ebd7ec55
mp.ashworth <mp.ashworth@gmail.com>
2011-07-18 16:29:13 +0000

    Issue 11:	Refactor the menu so that it is defined externally to the pages or layout page

---
+++ yellowfire/yellowfire-web/src/main/java/za/co/yellowfire/ui/training/IndexController.java
@@ -0,0 +1,13 @@
+package za.co.yellowfire.ui.training;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * @author Mark P Ashworth
+ * @version 0.0.1
+ */
+@RequestScoped @Named("trainingIndexController")
+public class IndexController extends AbstractTrainingUIController {
+    private static final long serialVersionUID = 1L;
+}
