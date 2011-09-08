4a4f3525b8938958d1317d4507c1696184156839
mp.ashworth@gmail.com <mp.ashworth@gmail.com@196ab5ce-ea74-618e-3489-2268b32586af>
2011-09-08 04:49:12 +0000

    Changed installation folder of Solr

--- trunk/yellowfire/deployments/08-deploy-solr.bat
+++ trunk/yellowfire/deployments/08-deploy-solr.bat
@@ -1,3 +1,3 @@
 @REM "Deploying Solr"
-CALL %GLASSFISH_HOME%\bin\asadmin --port 4949 create-system-properties solr.data.dir=C\:\\SDE\\solr\\data:solr.solr.home=C\:\\SDE\\solr
+CALL %GLASSFISH_HOME%\bin\asadmin --port 4949 create-system-properties solr.data.dir=C\:\\SDE\\servers\\solr\\data:solr.solr.home=C\:\\SDE\\servers\\solr
 CALL %GLASSFISH_HOME%\bin\asadmin --port 4949 deploy --force true --contextroot solr --upload true apache-solr-3.2.0.war
