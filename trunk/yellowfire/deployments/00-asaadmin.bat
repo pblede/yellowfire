9b82ceead6a88e2c7b2e972bc0c3dd5cbe5e7a15
mp.ashworth@gmail.com <mp.ashworth@gmail.com@196ab5ce-ea74-618e-3489-2268b32586af>
2012-01-20 08:04:06 +0000

    Various changes to make yellowfire portable between application servers. Upgrade libraries.

--- trunk/yellowfire/deployments/00-asaadmin.bat
+++ trunk/yellowfire/deployments/00-asaadmin.bat
@@ -5,9 +5,14 @@ CALL 04-create-jdbc-resource.bat
 CALL 05-create-mail-resource.bat
 CALL 07-create-jms-resource.bat
 CALL 06-create-auth-realm.bat
+
+CALL 08-deploy-hornetq-2.bat
+CALL 10-stop-domain.bat
+CALL 03-start-domain.bat
+CALL 08-deploy-hornetq-3.bat
+
 CALL 08-deploy-solr.bat
 CALL 09-deploy-yellowfire.bat
 
 CALL 10-stop-domain.bat
-
 CALL 03-start-domain.bat
--- trunk/yellowfire/deployments/08-deploy-solr.bat
+++ trunk/yellowfire/deployments/08-deploy-solr.bat
@@ -1,3 +1,3 @@
 @REM "Deploying Solr"
 CALL %GLASSFISH_HOME%\bin\asadmin --port 4949 create-system-properties solr.data.dir=C\:\\SDE\\servers\\solr\\data:solr.solr.home=C\:\\SDE\\servers\\solr
-CALL %GLASSFISH_HOME%\bin\asadmin --port 4949 deploy --force true --contextroot solr --upload true apache-solr-3.2.0.war
+CALL %GLASSFISH_HOME%\bin\asadmin --port 4949 deploy --force true --contextroot solr --upload true .\\applications\\apache-solr-3.4.0.war
